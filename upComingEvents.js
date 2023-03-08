b0c13c512fd4b3509b4f888e761f346f851e6866
ceugeniag <gonzalezceugenia@gmail.com>
2023-03-08 18:43:54 -0300

    filtros

--- upComingEvents.js
+++ upComingEvents.js
@@ -18,7 +18,7 @@ function crearTarjetaConInner(event){
     <div class="card-body">
         <h5 class="card-title"> ${ event.name } </h5>
         <p class="card-text">${ event.date }</p>
-        <a href="./details.html" class="btn btn-danger">More details</a>
+        <a href="./details.html?id=${event._id}" class="btn btn-danger">More details</a>
         </div>
     </div> ` 
     return template;
@@ -29,9 +29,78 @@ let templateAcumuladas="";
 for(let event of future){
     templateAcumuladas += crearTarjetaConInner(event)
 }
+
+function pintarTarjetas( lista, elemento ){
+    let template = ''
+    lista.forEach( personaje => template += crearTarjetaConInner( personaje ) )
+    elemento.innerHTML = template
+}
+
+
 ////Busco la section donde esta lo que quiero agregar
 const section = document.getElementById( "sectionFuture" )
 
 
 //Reasignar el valor a section
 sectionFuture.innerHTML=templateAcumuladas;
+
+
+//------------------------- AHORA FILTROS ---------------------------
+
+
+//Busco la section donde esta lo que quiero agregar
+const $checkbox = document.getElementById( "check" );
+
+
+//Creo la lista de categorias
+const listaCategory = Array.from(new Set(future.map(category => category.category)))
+
+
+// las llevo al html
+const categorias= listaCategory.reduce( (acc, category) => acc += `
+<label class="form-check-label px-3" for="flexCheckChecked"> ${category}</label> <input class="form-check-input " type="checkbox" value="${category}" id="flexCheckChecked" >`, '')
+$checkbox.innerHTML+= categorias
+
+
+//Aca creo el evento
+$checkbox.addEventListener("change", e => {
+    const filtrado = filtroCruzado()
+    pintarTarjetas(filtrado, sectionFuture)
+})
+
+pintarTarjetas(future, sectionFuture)
+
+
+// Aca armo la funcion  para filtrar con los checks
+function filtroCheck(listaEventos){
+    const check= document.querySelectorAll( 'input[type="checkbox"]:checked')
+    const arraycheckbox = Array.from(check).map(checkbox=> checkbox.value )
+    if(arraycheckbox.length === 0){
+        return listaEventos;
+    } else {
+        return  listaEventos.filter(event => arraycheckbox.includes(event.category))
+    }
+}
+
+//Busco del html
+const $search= document.getElementById("search");
+
+
+//creo el evento y le aplico el filtrado
+$search.addEventListener('input', e=>{
+    const filtrado = filtroCruzado()
+    pintarTarjetas(filtrado, sectionFuture)
+})
+
+//Esta funcion me va capturando letra por letra
+
+function filtroPalabras(lista) {
+    const ingresaUsuario = $search.value.toLowerCase();
+    const filtro= lista.filter(event=> event.name.toLowerCase().includes(ingresaUsuario))
+    return filtro;
+}
+
+//Filtro cruzado a ver que onda
+function filtroCruzado(){
+        return filtroPalabras(filtroCheck(future))
+}
